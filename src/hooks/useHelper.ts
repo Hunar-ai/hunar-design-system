2bc719874aa6b7bff6004b8a57186304ff4815af
Amarjeet Kumar <amar-hunar@users.noreply.github.com>
2024-06-06 09:19:19 +0530

    fix: :bug: custom select

--- src/hooks/useHelper.ts
+++ src/hooks/useHelper.ts
@@ -1,21 +1,24 @@
+import React from 'react';
+
 import type { Option, Options } from '@/interfaces';
 
 export const useHelper = () => {
-    const getValueToLabelMap = (
-        options: Options
-    ): { [key: string]: string } => {
-        const optionsMap = options.reduce(
-            (acc: { [key: string]: string }, key: Option) => {
-                acc = {
-                    ...acc,
-                    [key.value]: key.label
-                };
-                return acc;
-            },
-            {}
-        );
-        return optionsMap;
-    };
+    const getValueToLabelMap = React.useCallback(
+        (options: Options): { [key: string]: string } => {
+            const optionsMap = options.reduce(
+                (acc: { [key: string]: string }, key: Option) => {
+                    acc = {
+                        ...acc,
+                        [key.value]: key.label
+                    };
+                    return acc;
+                },
+                {}
+            );
+            return optionsMap;
+        },
+        []
+    );
 
     return { getValueToLabelMap };
 };
