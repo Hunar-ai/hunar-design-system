159f5d559b553734b888400c1a1c38ced879aeb3
Amarjeet Kumar <amar-hunar@users.noreply.github.com>
2024-07-10 06:48:11 +0530

    feat: :sparkles: add `DotDivider` component

---
+++ src/components/DotDivider/DotDivider.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { StorySection } from '@/components/storybook';
+import { DotDivider } from './DotDivider';
+import { Box } from '@mui/material';
+
+const meta = {
+    title: 'Components/DotDivider',
+    component: DotDivider,
+    parameters: { controls: { expanded: true } },
+    argTypes: {
+        fontSize: {
+            table: { type: { summary: `number | string` } },
+            control: {
+                type: 'number',
+                min: 0,
+                step: 1
+            }
+        }
+    },
+    args: { fontSize: 6 }
+} satisfies Meta<typeof DotDivider>;
+
+export default meta;
+
+type StoryProps = StoryObj<typeof DotDivider>;
+
+export const Playground: StoryProps = {
+    render: function Playground(props) {
+        return (
+            <StorySection
+                title=""
+                // eslint-disable-next-line max-len
+                description={`Change various props in the "Controls" panel to see how they change behavior of the component`}
+            >
+                <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    width="100%"
+                    columnGap={0.5}
+                >
+                    Apple <DotDivider {...props} />
+                    Mango <DotDivider {...props} />
+                    Orange
+                </Box>
+            </StorySection>
+        );
+    }
+};
