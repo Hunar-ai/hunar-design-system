e3f97f12e181dca5174f1070ef33ece92518805e
Amarjeet Kumar <amar-hunar@users.noreply.github.com>
2024-09-16 16:43:34 +0530

    feat: :sparkles: update storybook

--- src/components/SearchBar/SearchBar.stories.tsx
+++ src/components/SearchBar/SearchBar.stories.tsx
@@ -3,15 +3,35 @@ import { action } from '@storybook/addon-actions';
 
 import { Box } from '@mui/material';
 
-import { SearchBar } from './SearchBar';
+import { StorySection } from '@/components/storybook';
+import { SearchBar, type SearchBarProps } from './SearchBar';
 
 const setSearchValue = action('setSearchValue');
+
+interface SearchBarSectionProps extends SearchBarProps {
+    sectionTitle: string;
+    sectionDescription?: string;
+}
+
+const SearchBarSection = ({
+    sectionTitle,
+    sectionDescription,
+    ...props
+}: SearchBarSectionProps) => {
+    return (
+        <StorySection title={sectionTitle} description={sectionDescription}>
+            <Box width="100%" textAlign="end">
+                <SearchBar {...props} />
+            </Box>
+        </StorySection>
+    );
+};
 
 const meta = {
     title: 'Components/SearchBar',
     component: SearchBar,
     parameters: { controls: { expanded: true } },
-    args: { setSearchValue, mobileSearchWidth: 'calc(100vw - 88px)' }
+    args: { setSearchValue, xsWidth: 'calc(100vw - 88px)' }
 } satisfies Meta<typeof SearchBar>;
 
 export default meta;
@@ -19,17 +39,40 @@ export default meta;
 type StoryProps = StoryObj<typeof SearchBar>;
 
 export const Playground: StoryProps = {
-    parameters: {
-        description:
-            'Change various props in the "Controls" panel to see how they change behavior of the component',
-        hasDefaultSection: true
-    },
     decorators: Story => (
-        <Box width="100%" textAlign="end">
-            <Story />
-        </Box>
+        <StorySection
+            title=""
+            description='Change various props in the "Controls" panel to see how they change behavior of the component'
+        >
+            <Box width="100%" textAlign="end">
+                <Story />
+            </Box>
+        </StorySection>
     ),
     render: function Playground(props) {
         return <SearchBar {...props} />;
+    }
+};
+
+export const States: StoryProps = {
+    parameters: { controls: { exclude: ['placeholder', 'showMobileSearch'] } },
+    render: function States(props) {
+        return (
+            <>
+                <SearchBarSection sectionTitle="Default" {...props} />
+                <SearchBarSection
+                    sectionTitle="Placeholder"
+                    {...props}
+                    placeholder="Type here..."
+                />
+                <SearchBarSection
+                    sectionTitle="Full width SearchBar on Mobile"
+                    // eslint-disable-next-line max-len
+                    sectionDescription="Set `showMobileSearch=false` to show desktop style SearchBar on Mobile. (Change screen size to mobile to note difference)"
+                    {...props}
+                    showMobileSearch={false}
+                />
+            </>
+        );
     }
 };
