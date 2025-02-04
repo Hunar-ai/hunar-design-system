959317bb7b9655c7bf6ff5536ce2cbba4090ae93
Amarjeet Kumar <amar-hunar@users.noreply.github.com>
2025-02-04 17:12:16 +0530

    WIP: :construction: remove file

--- src/components/UploadButton/index.ts
+++ src/components/UploadButton/index.ts
@@ -1,5 +1,1 @@
 export { UploadButton, type UploadButtonProps } from './UploadButton';
-export {
-    UploadFilenamePreview as UploadInputPreview,
-    type UploadFilenamePreviewProps as UploadInputPreviewProps
-} from './UploadFilenamePreview';
